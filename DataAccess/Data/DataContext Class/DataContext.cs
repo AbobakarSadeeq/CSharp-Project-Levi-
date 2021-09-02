1d22b185df502ddd0f8e2c6b5906a0de86972648
AbobakarSadeeq <61661676+AbobakarSadeeq@users.noreply.github.com>
2021-09-02 12:48:59 +0500

    database connection added
    database finally added

---
+++ DataAccess/Data/DataContext Class/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Bussiness_Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data.DataContext_Class
+{
+   public class DataContext : DbContext
+   {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        // Registration of Entity Classes in Database
+
+        public DbSet<Category> Categories { get; set; }
+   }
+}
